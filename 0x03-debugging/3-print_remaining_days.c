5ddb8ca9800a44734d1edac50f7824ca76e1142c
lilly-njanja <njanjalillian@gmail.com>
2022-09-15 13:42:25 -0700

    remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,9 +1,10 @@
+#include "main.h"
 
 /**
 * print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
+* left in the yeah, taking leap years into account
 * @month: month in number format
-* @day: day of the month
+* @day: day of month
 * @year: year
 * Return: void
 */
@@ -24,7 +25,7 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf(Ã(("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
